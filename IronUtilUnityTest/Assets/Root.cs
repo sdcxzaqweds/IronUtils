a755ebbba615ff86581a250f8efcd1dab6076505
sdcxzaqweds <sdcxzaqweds@126.com>
2022-01-02 02:03:38 +0800

    打印test Unity vision

---
+++ IronUtilUnityTest/Assets/Root.cs
@@ -0,0 +1,29 @@
+﻿
+public class Root
+{
+    public void Init()
+    {
+        this.Log("InitRootLog.");
+        Mgr mgr = new Mgr();
+        mgr.Init();
+    }
+}
+
+public class Mgr
+{
+    public void Init()
+    {
+        this.Warn("Init Mgr warn.");
+        Item item = new Item();
+        item.Init();
+    }
+}
+
+public class Item
+{
+    public void Init()
+    {
+        this.Error("Init Item Error");
+        this.Trace("Trace this Func");
+    }
+}
---
+++ IronUtilUnityTest/Assets/TestLog.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IronUtils;
+using UnityEngine;
+
+public class TestLog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    { 
+        LogConfig config = new LogConfig()
+        {
+            enableLog = true,
+            LogPrefix = "",
+            enableTime = true,
+            logSeparate = ">",
+            enableThreadID = true,
+            enableTrace = false,
+            enableSave = true,
+            enableCover = true,
+            loggerType = LoggerType.Unity,
+            SavePath = Application.persistentDataPath + "/IronLog/",
+            SaveName = "ClientPELog.txt"
+        };
+        
+        IronLog.InitSetting(config);
+        IronLog.Log("{0} START...","SeverIronLog");
+        IronLog.ColorLog(LogColor.Red, "Color Log:{0}", LogColor.Red.ToString());
+        IronLog.ColorLog(LogColor.Blue, "Color Log:{0}", LogColor.Blue.ToString());
+        IronLog.ColorLog(LogColor.Green, "Color Log:{0}", LogColor.Green.ToString());
+        IronLog.ColorLog(LogColor.Cyan, "Color Log:{0}", LogColor.Cyan.ToString());
+        IronLog.ColorLog(LogColor.Magenta, "Color Log:{0}", LogColor.Magenta.ToString());
+        IronLog.ColorLog(LogColor.Yellow, "Color Log:{0}", LogColor.Yellow.ToString());
+        
+        Root root = new Root();
+        root.Init();
+    }
+    
+}
