42c7dc8e93d2621bb3efb1f15e9e1bfec5a93e5b
sdcxzaqweds <sdcxzaqweds@126.com>
2022-01-02 02:08:52 +0800

    C# 测试项目

---
+++ ServerLogTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using IronUtils;
+
+namespace ServerLogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IronLog.InitSetting();
+            IronLog.Log("{0} START...","SeverIronLog");
+            IronLog.ColorLog(LogColor.Red, "Color Log:{0}", LogColor.Red.ToString());
+            IronLog.ColorLog(LogColor.Blue, "Color Log:{0}", LogColor.Blue.ToString());
+            IronLog.ColorLog(LogColor.Green, "Color Log:{0}", LogColor.Green.ToString());
+            IronLog.ColorLog(LogColor.Cyan, "Color Log:{0}", LogColor.Cyan.ToString());
+            IronLog.ColorLog(LogColor.Magenta, "Color Log:{0}", LogColor.Magenta.ToString());
+            IronLog.ColorLog(LogColor.Yellow, "Color Log:{0}", LogColor.Yellow.ToString());
+            
+            Root root = new Root();
+            root.Init();
+        }
+    }
+}
---
+++ ServerLogTest/Root.cs
@@ -0,0 +1,31 @@
+﻿namespace ServerLogTest
+{
+    class Root
+    {
+        public void Init()
+        {
+            this.Log("InitRootLog.");
+            Mgr mgr = new Mgr();
+            mgr.Init();
+        }
+    }
+
+    class Mgr
+    {
+        public void Init()
+        {
+            this.Warn("Init Mgr warn.");
+            Item item = new Item();
+            item.Init();
+        }
+    }
+
+    class Item
+    {
+        public void Init()
+        {
+            this.Error("Init Item Error");
+            this.Trace("Trace this Func");
+        }
+    }
+}
